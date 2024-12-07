1a6093e69fa2fb2415f509c4c372ba1d7fb4e997
Michael Smith <michaelsmith1994@yahoo.com>
2024-12-07 00:38:08 +0000

    Another lab completed. **Sorry if there are extra submissions, had to reinstall the repository.**

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+@SuppressWarnings("unused")
 public class Lab {
     /**
      * Return a string with an array's contents, formatted with spaces.
@@ -32,6 +33,11 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str = "";
+        for(int i = 0; i < nums.length; i++){
+            str += nums[i];
+            str += ' ';
+        }
+        return str;
     }
 }
